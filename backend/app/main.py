d8f9aa6a1cffae2006f8c4ff52350cd39f6bcb73
SaiNiranjan2124 <pothinasai8906@gmail.com>
2026-03-10 14:30:59 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-import os
+import OS
 import sys
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -47,4 +47,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("backend.app.main:app", host="127.0.0.1", port=8000, reload=True)
